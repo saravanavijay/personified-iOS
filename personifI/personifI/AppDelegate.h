f32bb731c15168cc9a382411784a4f1b242f4146
saravanavijay <saravana.vijay.kumar@gmail.com>
2015-01-24 13:33:19 +0530

    First commit -a

---
+++ personifI/personifI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  personifI
+//
+//  Created by Vijay on 24/01/15.
+//  Copyright (c) 2015 personifIInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ personifI/personifI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  personifI
+//
+//  Created by Vijay on 24/01/15.
+//  Copyright (c) 2015 personifIInc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
